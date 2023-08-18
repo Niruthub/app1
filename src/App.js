0e63ebe4e446883179403152e3a733c49065a2ed
NirutKunwong <s6303051623055@email.kmutnb.ac.th>
2023-08-18 15:26:05 +0700

    add event-data.js
    create file and clear app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react'
-// import './style.css'
-import { Header,Content,Footer } from './func-components'
-import Calendar from './class-components'
-import Banner from './banner'
-import { Calculator2 } from './calculator'
+import { EventData2 } from './event-data'
+
 
 
 
 
 export default function App(){
-  return <Calculator2/>
+  return <EventData2/>
 }
