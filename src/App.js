392090f7f9a5cd1a2dae3b930e3362b2de49bb4f
NirutKunwong <s6303051623055@email.kmutnb.ac.th>
2023-08-18 14:00:54 +0700

    add banners

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import React from 'react'
 // import './style.css'
 import { Header,Content,Footer } from './func-components'
 import Calendar from './class-components'
+import Banner from './banner'
 
-function App() {
-  // แบบที่ 1
-  // return (
-  //  [<Header />,<Content />,<Footer />]
-  // )
-  // แบบที่ 2
-    return(
-      <>
-        <Header/>
-        <p><center><Calendar/></center></p> 
-        <Content/>
-        <Footer/>
-      </>
-    )
 
+
+
+export default function App(){
+  return <Banner/>
 }
-
-export default App;
