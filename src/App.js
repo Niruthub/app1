3f99e574ea20ba74944ab69f90faf4ad63d86fd0
NirutKunwong <s6303051623055@email.kmutnb.ac.th>
2023-09-22 15:21:35 +0700

    งาน React Store

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ import Content from './context-content'
 import Header2 from './context-header2'
 import { Component2 } from 'react'
 import Content2 from './context-content2'
+import Router2 from './router2'
+import Router3 from './router3'
 
 
 export default function App(){
-  let [user,setUser] = React.useState('');
   return (
-    <userContext.Provider value={[user,setUser]}>
-      <Header2/>
-      <Content2/>
-    </userContext.Provider>
+    <Router3/>
     )
 }
