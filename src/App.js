012cf41e16e5c0eadb1140182a805a360a6116d6
NirutKunwong <s6303051623055@email.kmutnb.ac.th>
2023-09-29 14:31:47 +0700

    add form-txt.js

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ import { Component2 } from 'react'
 import Content2 from './context-content2'
 import Router2 from './router2'
 import Router3 from './router3'
+import FormText from './form-text'
 
 
 export default function App(){
   return (
-    <Router3/>
+    <FormText/>
     )
 }
