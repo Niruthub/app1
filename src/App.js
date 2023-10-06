3e4788833749837253bd9e2750a377c0092edada
NirutKunwong <s6303051623055@email.kmutnb.ac.th>
2023-10-06 15:24:36 +0700

    create form-validation.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import React from 'react'
 // import RefsFunc from './refs-func'
 // import { EventData2 } from './event-data'
 // import MessageBox from './state-func'
-import {userContext} from './context'
-import Header from './context-header'
-import Content from './context-content'
-import Header2 from './context-header2'
-import { Component2 } from 'react'
-import Content2 from './context-content2'
-import Router2 from './router2'
-import Router3 from './router3'
-import FormText from './form-text'
-import FormCheck from './form-check'
+// import {userContext} from './context'
+// import Header from './context-header'
+// import Content from './context-content'
+// import Header2 from './context-header2'
+// import { Component2 } from 'react'
+// import Content2 from './context-content2'
+// import Router2 from './router2'
+// import Router3 from './router3'
+// import FormText from './form-text'
+// import FormCheck from './form-check'
+import FormValidation from './form-validation'
 
 
 export default function App(){
   return (
-    <FormCheck/>
+    <FormValidation/>
     )
 }
